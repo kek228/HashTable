a0cabb32ddbd55f3e8ef20661bcb474f13ba973b
kek228 <xyets777@gmail.com>
2019-11-04 21:20:06 +0300

    написал тест на кэш

--- testLFU.cpp
+++ testLFU.cpp
@@ -10,10 +10,24 @@ TEST(LFUTests, trivial) {
     LFUCache<string> lfu(2);
     lfu.insert("aaa");
     auto keys = lfu.topKeys();
-    for(auto &ck: keys){
-        ASSERT_EQ(ck.first, 1);
-        for(auto &k: ck.second)
-            ASSERT_EQ(k, "aaa");
-    }
-
+    auto counter1 = keys.find(1);
+    ASSERT_EQ(counter1->first, 1);
+    ASSERT_EQ(counter1->second.size(), 1);
+    ASSERT_EQ(counter1->second[0], "aaa");
+    //
+    lfu.insert("bbb");
+    keys = lfu.topKeys();
+    counter1 = keys.find(1);
+    ASSERT_EQ(counter1->first, 1);
+    ASSERT_EQ(counter1->second.size(), 2);
+    ASSERT_EQ(counter1->second[0], "bbb");
+    ASSERT_EQ(counter1->second[1], "aaa");
+    //
+    lfu.insert("ccc");
+    keys = lfu.topKeys();
+    counter1 = keys.find(1);
+    ASSERT_EQ(counter1->first, 1);
+    ASSERT_EQ(counter1->second.size(), 2);
+    ASSERT_EQ(counter1->second[0], "ccc");
+    ASSERT_EQ(counter1->second[1], "bbb");
 }
